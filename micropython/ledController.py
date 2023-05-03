b0ec889c1a6edf02fcf7eb10a4ace46389a77eaf
Ian Huber <huberiah@gmail.com>
2023-05-03 17:00:34 -0600

    all base modes functional

--- micropython/ledController.py
+++ micropython/ledController.py
@@ -1,10 +1,13 @@
 from neopixel import NeoPixel
 from machine import Pin
-
+import random, math
 # TODO Color Palette Control
 
 class LedController:
     NUM_LEDS = 60
+    ENC_TO_HSV_MAP = 1270
+    MAX_HUE = 65536
+    
     def __init__(self, led_pin: int):
         
         # Initalize led_pin
@@ -16,37 +19,191 @@ class LedController:
     def rainbow(self):
         pass
 
-    def setPalette(self, palette):
+    def set_palette(self, palette):
         self.palette = palette
 
-    def ledClock(self, time) -> None:
-        hour = time[0]
-        minute = time[1]
-        second = time[2]
+    def led_clock(self, time, enc_val) -> None:
+        hour = (time[0] * 5) - 31
+        minute = time[1] - 31
+        second = time[2] - 31
+        start_color = enc_val * self.ENC_TO_HSV_MAP
         
+        hour_color = self.colorHSV(start_color, 255, 255)
+        min_sec_color = self.colorHSV(start_color - 2500, 255, 255)
+
         for i in range(self.NUM_LEDS):
             self.leds[i] = (0, 0, 0)
         
-        self.leds[hour] = (255, 255, 255)
-        self.leds[hour-1] = (255, 255, 255)
-        self.leds[hour+1] = (255, 255, 255)
-        self.leds[minute] = (255, 0, 0)
-        self.leds[second] = (0, 255, 255)
+        self.leds[hour] = hour_color
+        self.leds[hour - 1] = hour_color
+        self.leds[hour + 1] = hour_color
+        self.leds[minute] = min_sec_color
+        self.leds[second] = min_sec_color
+        self.leds.write()
+    
+    def fire(self, enc_val) -> None:
+        start_color = enc_val * self.ENC_TO_HSV_MAP
+        for i in range(self.NUM_LEDS):
+            #color = self.colorHSV(5000, random.randrange(0, 255), random.randrange(0, 255))
+            color = self.colorHSV(random.randrange(start_color, start_color + 2000), 255, random.randrange(0, 255))
+            self.leds[i] = color
+        self.leds.write()
+    
+    def static(self, enc_val) -> None:
+        start_color = enc_val * self.ENC_TO_HSV_MAP
+        color = self.colorHSV(start_color, 255, 255)
+        self.leds.fill(color)
+        self.leds.write()
+        pass
+
+    def colorwave(self, enc_val, start_led, hue):
+        wave_leds = math.floor( enc_val * 0.4 ) + 1
+        brightness_delta = math.floor( 255 / wave_leds )
+        hue_delta = math.floor( self.MAX_HUE / 250 )
+        self.clear()
+        
+        led_num = 0
+        for i in range(start_led, start_led - wave_leds, -1):
+            color = self.colorHSV(hue, 255, 255 - ( brightness_delta * led_num ) )
+            self.leds[i] = color
+            led_num += 1
+        self.leds.write()
+        
+        start_led += 1
+        if start_led > 59:
+            start_led = 0
+            
+        hue += hue_delta
+        if hue > self.MAX_HUE:
+            hue = 0
+            
+        return start_led, hue
+
+    def rainbow(self, enc_val, start_led):
+        saturation = math.floor( 255 - (enc_val * 4) )
+        led_num = 0
+        for i in range(start_led, start_led - 59, -1):
+            hue = math.floor( ( self.MAX_HUE / 60) * led_num )
+            color = self.colorHSV(hue, saturation, 255)
+            self.leds[i] = color
+            led_num += 1
+        self.leds.write()
+        
+        start_led += 1
+        if start_led > 59:
+            start_led = 0
+        
+        return start_led
+
+    def kaleidoscope(self, enc_val, first_color):
+        hue_delta = math.floor(self.MAX_HUE / 2)
+        rate = math.floor(2570 / 2)
+        num_patterns = int((enc_val / 5) + 2)
+        first_color += rate
+        if first_color > self.MAX_HUE:
+            first_color = 0
+        last_color = first_color + hue_delta
+        if last_color > self.MAX_HUE:
+            last_color = first_color + hue_delta - self.MAX_HUE
+        
+        print(enc_val, num_patterns)
+        
+        for i in range(0, num_patterns):
+
+            if i % 2 == 0:
+                for j in range(0, math.floor(self.NUM_LEDS / num_patterns)):
+                    color = self.colorHSV(first_color - (2500 * j), 255, 255)
+                    self.leds[(i * math.floor((self.NUM_LEDS / num_patterns))) + j] = color
+            if i % 2 == 1:
+                for j in range(0, math.floor(self.NUM_LEDS / num_patterns)):
+                    color = self.colorHSV(last_color - (2500 * j), 255, 255)
+                    self.leds[(i * math.floor((self.NUM_LEDS / num_patterns))) + j] = color
+
+        self.leds.write()
+        
+        return first_color
+        
+    
+    def ble(self) -> None:
+        current_state = self.leds[-1]
+        if current_state[2] > 0:
+            for i in range(self.NUM_LEDS):
+                self.leds[i] = (0, 0, 0)
+        else:
+            self.leds[-1] = (0, 0, 255)
+            self.leds[14] = (0, 0, 255)
+            self.leds[29] = (0, 0, 255)
+            self.leds[44] = (0, 0, 255)
         self.leds.write()
 
-    def killLeds(self):
-        for i in range(self.NUM_LEDS):
-            self.leds[i] = (0, 0, 0)
-            self.leds.write()
+    def clear(self):
+        self.leds.fill((0, 0, 0))
+        self.leds.write()
 
     def hue():
         pass
+
+    def colorHSV(self, hue, sat, val):
+        """
+        Converts HSV color to rgb tuple and returns it.
+        The logic is almost the same as in Adafruit NeoPixel library:
+        https://github.com/adafruit/Adafruit_NeoPixel so all the credits for that
+        go directly to them (license: https://github.com/adafruit/Adafruit_NeoPixel/blob/master/COPYING)
+        :param hue: Hue component. Should be on interval 0-65535
+        :param sat: Saturation component. Should be on interval 0-255
+        :param val: Value component. Should be on interval 0-255
+        :return: (r, g, b) tuple
+        """
+        if hue >= self.MAX_HUE:
+            hue %= self.MAX_HUE
+
+        hue = (hue * 1530 + 32768) // self.MAX_HUE
+        if hue < 510:
+            b = 0
+            if hue < 255:
+                r = 255
+                g = hue
+            else:
+                r = 510 - hue
+                g = 255
+        elif hue < 1020:
+            r = 0
+            if hue < 765:
+                g = 255
+                b = hue - 510
+            else:
+                g = 1020 - hue
+                b = 255
+        elif hue < 1530:
+            g = 0
+            if hue < 1275:
+                r = hue - 1020
+                b = 255
+            else:
+                r = 255
+                b = 1530 - hue
+        else:
+            r = 255
+            g = 0
+            b = 0
+
+        v1 = 1 + val
+        s1 = 1 + sat
+        s2 = 255 - sat
+
+        r = ((((r * s1) >> 8) + s2) * v1) >> 8
+        g = ((((g * s1) >> 8) + s2) * v1) >> 8
+        b = ((((b * s1) >> 8) + s2) * v1) >> 8
+
+        return r, g, b
+
 
 def main():
     pass
 
 if __name__ == '__main__':
     main()
+
 
 
 
--- micropython/main.py
+++ micropython/main.py
@@ -1,8 +1,7 @@
-from clock import Clock
 from peripherals import Peripherals
-from ledController import LEDController
-from espnetwork import ESPNetwork
-import date
+from ledController import LedController
+from espnetwork import EspNetwork
+import utime, ntptime
 
 # TODO implement secondary function for encoder based on button press
 
@@ -10,62 +9,129 @@ class infinityMirror():
     """
     Application level class for managing infinity mirror clock, LEDs, and Peripherals
     """
+    # mode constants
+    NUM_MODES = 6
     MODE_CLOCK = 0
-    MODE_STATIC = 1
-    MODE_RAINBOW = 2
+    MODE_FIRE = 1
+    MODE_STATIC = 2
     MODE_COLORWAVE = 3
-    MODE_KALEIDOSCOPE = 4
+    MODE_RAINBOW = 4
+    MODE_KALEIDOSCOPE = 5
+    
+    # timing constants
+    NTP_ATTEMPTS = 3
+    BLE_TIME_MS = 3000
+    DEBOUNCE_TIME_MS = 20
+    LOOP_DELAY_MS = (1 / 20) * 1000
+    BLE_LOOP_DELAY_MS = 500
 
     def __init__(self):
-        self.clock = Clock()
-        self.leds = LEDController()
-        self.peripherals = Peripherals()
-        self.espnetwork = ESPNetwork()
+        self.espNetwork = EspNetwork()
+        self.leds = LedController(33)
+        self.peripherals = Peripherals(34, 35, 25)
+        #TODO timezone should not be hardcoded
+        self.timezone = -6
+        self.led_mode = 0
+        self.ble = False
+        
+        # button handling
+        self.btn_was = 1
+        self.btn_dwn_time = None
+    
+        # get time
+        self.espNetwork.connect()
+        self.set_time()
+        self.time = self.get_time()
+        self.espNetwork.disconnect()
 
-    def clock(self) -> None:
-        # TODO Update to utilize color palettes
-        now = self.clock.getTime()
-        self.leds.ledClock(now)
+    def set_time(self):
+        initial_time = utime.localtime()
+        attempts_remaining = self.NTP_ATTEMPTS
+        while attempts_remaining > 0:
+            try:
+                ntptime.settime()
+            except Exception as e: 
+                print(f'Exception: {e}, trying {attempts_remaining} more times')
+            attempts_remaining -= 1
 
-    def static(self) -> None:
-        pass
+    def get_time(self):
+        newTime = utime.localtime(utime.mktime(utime.localtime()) + self.timezone * 3600)
+        self.time = [newTime[3], newTime[4], newTime[5]]
+        
+    def handle_button(self):
+        btn_val = self.peripherals.btnPin.value()
 
-    def rainbow(self) -> None:
-        pass
-
-    def colorwave(self) -> None:
-        pass
-
-    def kaleidoscope(self) -> None:
-        pass
-
+        # button was just pressed
+        if btn_val == 0 and self.btn_was == 1:
+            self.btn_dwn_time = utime.ticks_ms()
+            
+        # button was released after debounce period
+        elif btn_val == 1 and self.btn_was == 0 and utime.ticks_ms() - self.btn_dwn_time > self.DEBOUNCE_TIME_MS and utime.ticks_ms() - self.btn_dwn_time < self.BLE_TIME_MS:
+            self.led_mode += 1
+            print(f'get mode: {self.led_mode} ')
+            if self.led_mode > self.NUM_MODES:
+                self.led_mode = 0
+                
+        # button was released after BLE period
+        elif btn_val == 1 and self.btn_was == 0 and (utime.ticks_ms() - self.btn_dwn_time) > self.BLE_TIME_MS:
+            self.ble = not self.ble
+            self.leds.clear()
+            print(f'BLE Mode: {self.ble}')
+        
+        self.btn_was = btn_val
+                
 
 def main():
     # boot
     infinity = infinityMirror()
-    # init RTC time using SNTP
-    infinity.espnetwork.connect()
-    infinity.clock.setRTCStartTime()
-    infinity.espnetwork.disconnect()
-
+    start_time = utime.ticks_ms()
+    infinity.handle_button()
+    enc_val = infinity.peripherals.rotaryEncoder.value()
+    
+    # init LED vars
+    # colorwave
+    start_led = 0
+    hue = 0
+    # 
+    first_color = 0
+    
     # main loop
     while True:
-        # get mode from encoder value
-        # disable ISR during read to avoid race condition
-        enc_val = infinity.peripherals.rotaryEncoder.btnVal()
-        
-        if enc_val == infinity.MODE_CLOCK:
-            infinity.clock()
-        elif enc_val == infinity.MODE_STATIC:
-            infinity.static()
-        elif enc_val == infinity.MODE_RAINBOW:
-            infinity.rainbow()
-        elif enc_val == infinity.MODE_COLORWAVE:
-            infinity.colorwave()
-        elif enc_val == infinity.MODE_KALEIDOSCOPE:
-            infinity.kaleidoscope()
 
-        time.sleep(50)
+        if utime.ticks_ms() - start_time > infinity.LOOP_DELAY_MS:
+            if not infinity.ble:
+
+                if infinity.led_mode == infinity.MODE_CLOCK:
+                    infinity.get_time()
+                    infinity.leds.led_clock(infinity.time, enc_val)
+                    
+                elif infinity.led_mode == infinity.MODE_FIRE:
+                    infinity.leds.fire(enc_val)
+                    
+                elif infinity.led_mode == infinity.MODE_STATIC:
+                    infinity.leds.static(enc_val)
+                    
+                elif infinity.led_mode == infinity.MODE_COLORWAVE:
+                    start_led, hue = infinity.leds.colorwave(enc_val, start_led, hue)
+                        
+                elif infinity.led_mode == infinity.MODE_RAINBOW:
+                    start_led = infinity.leds.rainbow(enc_val, start_led)               
+
+                elif infinity.led_mode == infinity.MODE_KALEIDOSCOPE:
+                    first_color = infinity.leds.kaleidoscope(enc_val, first_color)
+
+                # update time
+                start_time = utime.ticks_ms()
+                
+            # in BLE config mode, used for partitioning
+            else:
+                if utime.ticks_ms() - start_time > infinity.BLE_LOOP_DELAY_MS:
+                    infinity.leds.ble()
+                    # update time
+                    start_time = utime.ticks_ms()
+                    
+        infinity.handle_button()
+        enc_val = infinity.peripherals.rotaryEncoder.value()
 
 
 if __name__ == "__main__":
